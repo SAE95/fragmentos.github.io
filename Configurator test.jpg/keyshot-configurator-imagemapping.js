4960e85e333d1969685e539c3a9e14a86d0a48e9
SAE95 <design.agx@gmail.com>
2020-12-21 11:57:18 +0100

    Add Files

---
+++ Configurator test.jpg/keyshot-configurator-imagemapping.js
@@ -0,0 +1,33 @@
+imagemappingdata =
+[
+    {
+        "image": "Cpnfigurator test.7.1.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "5+181",
+            "6+63"
+        ]
+    },
+    {
+        "image": "Cpnfigurator test.7.2.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "5+126",
+            "6+95"
+        ]
+    },
+    {
+        "image": "Cpnfigurator test.7.3.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "5+126",
+            "6+53"
+        ]
+    }
+];
---
+++ Configurator test.jpg/keyshot-configurator-modeldata.js
@@ -0,0 +1,46 @@
+model =
+{
+    "materialgroups": [
+        {
+            "materials": [
+                {
+                    "checked": true,
+                    "icon": "material%20set%201%201.png",
+                    "multimaterials": [
+                        "6+63",
+                        "5+181"
+                    ],
+                    "name": "Material set 1 1"
+                },
+                {
+                    "icon": "material%20set%201%202.png",
+                    "multimaterials": [
+                        "6+95",
+                        "5+126"
+                    ],
+                    "name": "Material set 1 2"
+                },
+                {
+                    "icon": "material%20set%201%203.png",
+                    "multimaterials": [
+                        "6+53",
+                        "5+126"
+                    ],
+                    "name": "Material set 1 3"
+                }
+            ],
+            "name": "Material set 1"
+        }
+    ],
+    "products": [
+        {
+            "checked": true,
+            "materialgroups": [
+                "Material set 1"
+            ],
+            "modelset": 1005,
+            "name": "Default"
+        }
+    ],
+    "studios": []
+};
